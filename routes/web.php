88bc75ec6a41bf45c5619a507b01fa070fe1f8e9
Antony Mora <anthony07952011@gmail.com>
2023-11-08 20:47:40 -0500

    Set up a fresh Laravel app

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+// Route::post();
+// Route::put();
+// Route::delete();
+
+
+
+Route::middleware('auth')->group(function () {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/chirps', function () {
+        return view('chirps.index');
+    })->name('chirps.index');
+});
+
+require __DIR__.'/auth.php';

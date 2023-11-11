abfedf2443ee191cffd685d60c6e833bf34fe3f3
Antony Mora <anthony07952011@gmail.com>
2023-11-11 16:26:42 -0500

    16-11

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -9,13 +9,50 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form action="">
-                        <textarea name="messege" class="bg-transparent">
-
-                        </textarea>
-                        <button>Chirp</button>
+                    <form method="POST" action="{{ route('chirps.store') }}">
+                        @csrf
+                        <textarea name="message"
+                            class="block w-full rounded-md border-gray-300 bg-white shadow-sm transition-colors duration-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:border-indigo-300 dark:focus:ring dark:focus:ring-indigo-200 dark:focus:ring-opacity-50"
+                            placeholder="{{ __('Ingrese un Mensaje')}}"
+                        >{{ old('message') }}</textarea>
+                        <x-input-error :messages="$errors->get('message')" class="mt-2"/>
+                        <x-primary-button class="mt-4">
+                            {{ __('CHIRP')}}
+                        </x-primary-button>
                     </form>
                 </div>
+            </div>
+            <div class="mt-6 bg-white dark:bg-gray-800 shadow-sm rounded-lg divide-y dark:divide-gray-900">
+                @foreach ($chirps as $chirp)
+                    <div class="p-6 flex space-x-2">
+                    <svg class="h-6 w-6 text-gray-600 dark:text-gray-400 -scale-x-100" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 01-.825-.242m9.345-8.334a2.126 2.126 0 00-.476-.095 48.64 48.64 0 00-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0011.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"></path>
+                    </svg>
+                    <div class="flex-1">
+                        <div class="flex justify-between items-center">
+                            <div>
+                                <span class="text-gray-800 dark:text-gray-200">{{ $chirp -> user -> name }}</span>
+                                <small class="ml-2 text-sm text-gray-600 dark:text-gray-400">{{ $chirp -> created_at -> format('J M Y, g:i a') }}</small>
+                            </div>
+                        </div>
+                        <p class="mt-4 taxt-lg text-gray-900 dark:text-gray-100">{{ $chirp -> message }}</p>
+                    </div>
+                    <x-dropdown>
+                        <x-slot name="trigger">
+                            <button>
+                                <svg class="w-5 h-5 text-gray-500 dark:text-gray-300" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"></path>
+                                </svg>
+                            </button>
+                        </x-slot>
+                        <x-slot name="content">
+                            <x-dropdown-link href="{{ route('chirps.edit', $chirp) }}">
+                                Edit
+                            </x-dropdown-link>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+                @endforeach
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Chirp;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,9 +21,18 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/chirps', function () {
-        return view('chirps.index');
-    })->name('chirps.index');
+
+    Route::get('/chirps', [ChirpController::class, 'index'])
+    ->name('chirps.index');
+
+    Route::post('/chirps', [ChirpController::class, 'store'])
+    ->name('chirps.store');
+
+    Route::get('/chirps/{chirp}/edit', [ChirpController::class,'edit'])
+    ->name('chirps.edit');
+
+    Route::put('/chirps/{chirp}', [ChirpController::class,'update'])
+    ->name('chirps.update');
 });
 
 require __DIR__.'/auth.php';

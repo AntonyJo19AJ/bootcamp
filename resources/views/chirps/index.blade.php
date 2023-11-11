0c4125d10c35dc0cca9276ae7d69e308b3bf653b
Antony Mora <anthony07952011@gmail.com>
2023-11-11 16:25:38 -0500

    Prueba

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <form action="">
-                        <textarea name="messege" class="bg-transparent" cols="20" rows="10">
+                        <textarea name="messege" class="bg-transparent">
 
                         </textarea>
                         <button>Chirp</button>
